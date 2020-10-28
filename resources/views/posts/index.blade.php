c0158a716e1f7fcc403c1842217feb72054adc37
井上凌途 <inoueryoto@inoueryoutonoMacBook-puro.local>
2020-10-28 19:10:37 +0900

    add:トップ画面

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,18 +2,22 @@
 
 @section('content')
 
-    <h2>手と手整骨院予約サイト</h2>
-
-    <div class="rounded">
-        <h3>
-            手と手整骨院
-        </h3>
-        <p>
-            〒206-0802<br>
-            東京稲城市東長沼1052         TEL:042-407-5884<br>
-            定休日:日、祝日<br>
-            診療時間:9:00〜12:30  15:00〜20:00<br>
-        </p>   
+    <div class="text-center">
+        <h2>手と手整骨院予約サイト</h2>
+    </div>
+    <div class=" border-green text-left ">
+        
+            <h3 class="m-2">
+                手と手整骨院
+            </h3>
+        
+            <p class="m-2">
+                〒206-0802<br>
+                東京稲城市東長沼1052         TEL:042-407-5884<br>
+                定休日:日、祝日<br>
+                診療時間:9:00〜12:30  15:00〜20:00<br>
+            </p> 
+          
     </div>
 
 @endsection('content')
