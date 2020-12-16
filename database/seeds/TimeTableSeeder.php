dbd2a6916fb4535841f0df83a5c042550ef09aef
Takano <anpanmannoatarashiikao@gmail.com>
2020-12-16 16:55:03 +0900

    fix:細かな修正

--- database/seeds/TimeTableSeeder.php
+++ database/seeds/TimeTableSeeder.php
@@ -12,31 +12,31 @@ class TimeTableSeeder extends Seeder
     public function run()
     {
         DB::table('times')->insert([
-            'time_number' => '9:00',
+            'time_number' => '9:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '10:00',
+            'time_number' => '10:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '11:00',
+            'time_number' => '11:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '12:00',
+            'time_number' => '12:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '15:00',
+            'time_number' => '15:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '16:00',
+            'time_number' => '16:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '17:00',
+            'time_number' => '17:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '18:00',
+            'time_number' => '18:00~',
         ]);
         DB::table('times')->insert([
-            'time_number' => '19:00',
+            'time_number' => '19:00~',
         ]);
     }
 }
