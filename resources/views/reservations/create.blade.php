a766596028a2607ef6009136b3b56d8a1b29ac94
井上凌途 <inoueryoto@20N1201274ryotoinoue.local>
2021-01-27 13:22:52 +0900

    add:予約ボタン追加

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -1,9 +1,8 @@
 @extends('layout')
 
 @section('content')
-<div class="m-3">
+<div class="m-2">
 
-        <p class="m-2">
             ①選択した日程<br>
                 2020年4月30日<br>
             ②ご希望の時間を選択してください<br>
@@ -33,98 +32,68 @@
                         @endif
                     @endforeach
                 </div>
-                      
-                        
-                            <div class="btn-group">
-                                <label class="btn btn-outline-secondary ">
-                                    <input type="radio" name="options" id="option1" autocomplete="off"> 1
-                                </label>
-                                <label class="btn btn-outline-secondary">
-                                    <input type="radio" name="options" id="option2" autocomplete="off"> 2
-                                </label>
-                                <label class="btn btn-outline-secondary">
-                                    <input type="radio" name="options" id="option3" autocomplete="off"> 3
-                                </label>
-                            </div><br>
-                            
-                            <div class="btn-group"> 
-                                <label class="btn btn-outline-secondary ">
-                                    <input type="radio" name="options" id="option4" autocomplete="off" > 4
-                                </label>
-                                <label class="btn btn-outline-secondary">
-                                    <input type="radio" name="options" id="option5" autocomplete="off"> 5
-                                </label>
-                                <label class="btn btn-outline-secondary">
-                                    <input type="radio" name="options" id="option6" autocomplete="off"> 6
-                                </label>
-                            </div><br>
-                         
-                            <div class="btn-group">
-                                <label class="btn btn-outline-secondary ">
-                                    <input type="radio" name="options" id="option7" autocomplete="off" > 7                            
-                                </label>
-                                <label class="btn btn-outline-secondary">
-                                    <input type="radio" name="options" id="option8" autocomplete="off"> 8
-                                </label>
-                                <label class="btn btn-outline-secondary">
-                                    <input type="radio" name="options" id="option9" autocomplete="off"> 9
-                                </label>
-                            </div><br>                           
-                     
-
-                                
-
-                                
-                </div> 
+                    
             
            
-               ③ご希望のコースをお選びください<br>
-               <select class="form-control" id="selectEvalute" name="selectEvaluate">
-               <option>選択してください</option>
-               @foreach($courses as $key=>$course)
-                    @if((!empty($request->course_id) && $request->course_id == $course->id) || old('course_id') == $course->id )
-                        <option value="{{ $course->id }}" selected>{{ $course->course_name }}</option>
-                    @else
-                        <option value="{{ $course->id }}">{{ $course->course_name }}</option>
-                    @endif
-                @endforeach
+                <div class="mt-2">
+                    ③ご希望のコースをお選びください<br>
+                    <select class="form-control" id="selectEvalute" name="selectEvaluate">
+                    <option>選択してください</option>
+                
+                    @foreach($courses as $key=>$course)
+                        @if((!empty($request->course_id) && $request->course_id == $course->id) || old('course_id') == $course->id )
+                            <option value="{{ $course->id }}" selected>{{ $course->course_name }}</option>
+                        @else
+                            <option value="{{ $course->id }}">{{ $course->course_name }}</option>
+                        @endif
+                    @endforeach
+                    </select>
+                </div>
 
-        
-               </select>
-
-               <div>
-                  <p>④お客様の情報を入力してください</p>
+                <div class="mt-2">
+                  ④お客様の情報を入力してください
                   <div>
                      <p class="m-0">氏名</p>
                      {{Form::text('name', null, ['class' => 'form-control', 'placeholder' => '例)山田花子'])}}
                   </div>
-                  <p>年齢</p>
-                  {{Form::text('name', null, ['class' => 'form-control', 'placeholder' => '例)25'])}}
+                </div>
 
+
+                <div class="mt-2">
+                  年齢
+                  {{Form::text('name', null, ['class' => 'form-control', 'placeholder' => '例)25'])}}
+                </div>
+
+                <div class="mt-2">
                   性別<br>
                   {{Form::radio('gender', '1', false, ['class'=>''])}}
                   {{Form::label('gender','男性')}}
 
                   {{Form::radio('gender', '2', false, ['class'=>''])}}
-                  {{Form::label('gender','女性')}}<br>
+                  {{Form::label('gender','女性')}}
+                </div>
 
+                <div class="mt-2">
                   メールアドレス<br>
                   {{Form::text('name', null, ['class' => 'form-control', 'placeholder' => '例)sample@email.com'])}}
+                </div>
+
+                <div class="mt-2">
                   電話番号<br>
                   {{Form::text('name', null, ['class' => 'form-control', 'placeholder' => '例)09012345678'])}}
+               </div>
+
+               <div class="container mt-4 text-center">
+
+                    <a class="btn btn-primary" href="">
+                        予約確認
+                    </a>
 
                </div>
+
+
             {{Form::close()}}
-        </p> 
-
-
-
-
-
-
 
 </div>
-     
-
 
 @endsection('content')
