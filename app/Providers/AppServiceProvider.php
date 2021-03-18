5d9d07aa35912334a3e15c6b212c507113d49561
volcite <volcite.yuki.takano@gmail.com>
2021-03-19 00:38:45 +0900

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
+        
     }
 }
